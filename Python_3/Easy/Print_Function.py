dccfe728bcfe6f9e03f7085da6ffadd8dea57d20
Lam Phuoc Huy <80982762+NagiLam@users.noreply.github.com>
2021-03-24 22:42:05 +0100

    Update Print_Function.py

--- Python_3/Easy/Print_Function.py
+++ Python_3/Easy/Print_Function.py
@@ -5,10 +5,15 @@ Read an integer N. Without using any string methods, try to print the following:
 
 Note that "..." represents the values in between.
 
-Created on Sun Oct  7 21:10:42 2018
-@author: nagiAI
 """
+#Use end parameter
 n = int(input())
 for i in range(1, n+1):
     print(i, end="")
 
+#Use String concatenate
+n = int(input())
+temp = ""
+for i in range(n):
+    temp += str(i+1)
+    print(temp)
