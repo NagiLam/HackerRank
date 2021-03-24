236a5c722166298be989a5303afc778022af5977
Lam Phuoc Huy <80982762+NagiLam@users.noreply.github.com>
2021-03-24 22:12:04 +0100

    Update Arithmetic_Operators.py

--- Python_3/Easy/Arithmetic_Operators.py
+++ Python_3/Easy/Arithmetic_Operators.py
@@ -1,5 +1,5 @@
 """ Problem: Arithmetic Operators || Task:
-Read two integers from STDIN and print three lines where:
+Read two integers a and b from STDIN and print three lines where:
 
 1. The first line contains the sum of the two numbers.
 2. The second line contains the difference of the two numbers (first - second).
