b9f3e297565b77a32b70fac330f7f5912fbfb372
Lam Phuoc Huy <80982762+NagiLam@users.noreply.github.com>
2021-03-24 23:22:20 +0100

    Update and rename Find_the_Runner-Up_Score.py to Find_the_Runner_Up_Score.py

--- Python_3/Easy/Find_the_Runner_Up_Score.py
+++ Python_3/Easy/Find_the_Runner_Up_Score.py
@@ -2,9 +2,8 @@
 Given the participants' score sheet for your University Sports Day, you are required to find the runner-up score. 
 You are given n scores. Store them in a list and find the score of the runner-up.
 
-Created on Sun Oct  7 21:12:47 2018
-@author: nagiAI
 """
+#First solution
 n = int(input())
 arr = map(int, input().split())
 count = 0
@@ -17,3 +16,14 @@ for i in range(count):
     listArr.remove(max(listArr))
     
 print(max(listArr))
+
+#Second solution
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+    result = sorted(list(arr))
+    temp = 0
+    for i in result:
+        if i < max(result):
+            temp = i 
+    print(temp)
