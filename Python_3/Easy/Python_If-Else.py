744c48633c8abac1f627f3717ef7f0ec05d17bf7
Lam Phuoc Huy <80982762+NagiLam@users.noreply.github.com>
2021-03-24 22:03:25 +0100

    Update Python_If-Else.py

--- Python_3/Easy/Python_If-Else.py
+++ Python_3/Easy/Python_If-Else.py
@@ -2,9 +2,9 @@
 Given an integer, n, perform the following conditional actions:
 
 1. If n is odd, print Weird
-2. If n is even and in the inclusive range of  to , print Not Weird
-3. If n is even and in the inclusive range of  to , print Weird
-4. If n is even and greater than , print Not Weird
+2. If n is even and in the inclusive range of 2 to 5, print Not Weird
+3. If n is even and in the inclusive range of 6 to 20, print Weird
+4. If n is even and greater than 20, print Not Weird
 """
 
 N = int(input())
