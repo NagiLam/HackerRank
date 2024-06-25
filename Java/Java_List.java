cb6893148a05a9c5de61be7695f5a99754239dda
Lam Phuoc Huy <80982762+NagiLam@users.noreply.github.com>
2024-06-25 16:34:11 +0200

    Create Java_List.java

---
+++ Java/Java_List.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int sizeOfList = Integer.parseInt(in.nextLine());
+        List <Integer> listL = new ArrayList<Integer>(sizeOfList);
+
+        // Read a line contains space-separated integers describing elements into listL
+        String line = in.nextLine();
+        String[] elements = line.split(" ");
+        for (int i = 0; i < elements.length; i++){
+            listL.add(Integer.parseInt(elements[i]));
+        }
+
+        // Query part
+        int numberofQuery = Integer.parseInt(in.nextLine());
+        for (int i = 0; i < numberofQuery; i++){
+            String s = in.next();
+            if (s.equals("Insert")){
+                listL.add(in.nextInt(), in.nextInt());
+            }
+            if (s.equals("Delete")){
+                listL.remove(in.nextInt());
+            }
+        }
+
+        in.close();
+        for (int i = 0; i < listL.size(); i++) {
+            System.out.print(listL.get(i) + " ");
+        } 
+    }
+}
