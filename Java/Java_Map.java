aee57dd42c3935cd1f5651e411bd73476d0f1489
Lam Phuoc Huy <80982762+NagiLam@users.noreply.github.com>
2024-06-28 15:58:29 +0200

    Create Java_Map.java

---
+++ Java/Java_Map.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public static void main(String[] argh) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        in.nextLine();
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
+
+        for (int i = 0; i < n; i++) {
+            String name = in.nextLine();
+            int phone = in.nextInt();
+            map.put(name, phone);
+            in.nextLine();
+        }
+
+        while (in.hasNext()) {
+            String s = in.nextLine();
+            if (!map.containsKey(s)) {
+                System.out.println("Not found");
+            }
+            System.out.println(s + "=" + map.get(s));
+        }
+    }
+}
