cb015e3bb985355b7a6500498776b4371a886760
Lam Phuoc Huy <80982762+NagiLam@users.noreply.github.com>
2024-07-06 20:48:59 +0200

    Create day3.java

---
+++ 30Days_of_Code/day3.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+        bufferedReader.close();
+
+        if (N % 2 != 0) {
+            System.out.println("Weird");
+        }
+
+        if (N % 2 == 0 & N >= 2 && N <= 5) {
+            System.out.println("Not Weird");
+        }
+
+        if (N % 2 == 0 & N >= 6 && N <= 20) {
+            System.out.println("Weird");
+        }
+
+        if (N % 2 == 0 && N > 20) {
+            System.out.println("Not Weird");
+        }
+    }
+}
